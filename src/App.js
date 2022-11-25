5b110250a2c6a339543786a82b0a37bc0311c444
Oksana Oberemok <Pylypenkok@gmail.com>
2022-11-25 17:28:17 +0700

    create header with style

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <div>
-          <img src="" alt="" />
-          <h1>Search For A Song</h1>
-        </div>
-      </header>
+      <div className="container">
+        <header className="header">
+          <div className="main-title">
+            <img src="/images/notes.png" alt="musical notes" />
+            <h1>Search For A Song</h1>
+          </div>
+          <h3>Get the lyrics for any track</h3>
+          <form action="#" className="header-form">
+            <input
+              type="text"
+              placeholder="Song title..."
+              name="song-title"
+              className="header-form__input"
+            />
+            <button className="header-form__btn">Get Track Lyrics</button>
+          </form>
+        </header>
+      </div>
     </div>
   );
 }
