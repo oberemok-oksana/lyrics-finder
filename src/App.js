da1794bf59970d11467e4d6daa89257ddac8ddd9
Oksana Oberemok <Pylypenkok@gmail.com>
2022-11-29 19:27:19 +0700

    replace data with pages for router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Lyrics from "./components/Lyrics";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="header">
-          <div className="main-title">
-            <img src="/images/notes.png" alt="musical notes" />
-            <h1>Search For A Song</h1>
-          </div>
-          <h3>Get the lyrics for any track</h3>
-          <form action="#" className="header-form">
-            <input
-              type="text"
-              placeholder="Song title..."
-              name="song-title"
-              className="header-form__input"
-            />
-            <button className="header-form__btn">Get Track Lyrics</button>
-          </form>
-        </header>
-      </div>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <HomePage />
+      </Route>
+      <Route path="/lyrics/:trackId">
+        <Lyrics />
+      </Route>
+    </Switch>
   );
 }
 
