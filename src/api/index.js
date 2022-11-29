5a68a1218285f752aec529fca9ca5b8e0dfd3d4c
Oksana Oberemok <Pylypenkok@gmail.com>
2022-11-29 21:41:35 +0700

    implement findTrack,delete unnecessary code

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,3 @@
-//http://api.musixmatch.com/ws/1.1/chart.tracks.get?hot&page=1&page_size=10&country=it&f_has_lyrics=1
-
 export const getTopTracks = () => {
   return fetch(
     "http://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=a378662ebca7fe6e2eeec433bf0f805b&hot&page=1&page_size=10&country=us&f_has_lyrics=1"
@@ -16,7 +14,6 @@ export const getTrackInfo = (id) => {
     .then((data) => data.message.body.track);
 };
 
-// track.lyrics.get?track_id=15953433
 export const getTrackLyrics = (id) => {
   return fetch(
     `http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=a378662ebca7fe6e2eeec433bf0f805b&commontrack_id=${id}`
@@ -25,4 +22,12 @@ export const getTrackLyrics = (id) => {
     .then((data) => {
       return data.message.body.lyrics;
     });
+};
+
+export const findTrack = (text) => {
+  return fetch(
+    `http://api.musixmatch.com/ws/1.1/track.search?apikey=a378662ebca7fe6e2eeec433bf0f805b&q=${text}`
+  )
+    .then((response) => response.json())
+    .then((data) => data.message.body.track_list);
 };
